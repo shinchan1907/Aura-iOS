fa3e4296e9e67278db6c910e84ab56effb812e06
Sunny Gupta <sannygupta102@gmail.com>
2026-09-13 23:43:27 +0530

    Merge FocusAttributes definition to Core to fix Intent compilation

--- Core/Data/Models/FocusAttributes.swift
+++ Core/Data/Models/FocusAttributes.swift
@@ -3,20 +3,27 @@ import ActivityKit
 
 public struct FocusAttributes: ActivityAttributes {
     public struct ContentState: Codable, Hashable {
-        public var durationElapsed: TimeInterval
-        public var progress: Double // 0 to 1
+        public var sessionState: SessionState
+        public var elapsedSeconds: Int
+        public var totalEstimatedSeconds: Int?
         
-        public init(durationElapsed: TimeInterval, progress: Double) {
-            self.durationElapsed = durationElapsed
-            self.progress = progress
+        public init(sessionState: SessionState, elapsedSeconds: Int, totalEstimatedSeconds: Int? = nil) {
+            self.sessionState = sessionState
+            self.elapsedSeconds = elapsedSeconds
+            self.totalEstimatedSeconds = totalEstimatedSeconds
         }
     }
     
-    public var taskTitle: String
-    public var taskEstimatedDuration: TimeInterval?
+    public enum SessionState: String, Codable, Hashable {
+        case active = "Active"
+        case paused = "Paused"
+        case approachingTarget = "Approaching Target"
+        case completed = "Completed"
+    }
     
-    public init(taskTitle: String, taskEstimatedDuration: TimeInterval? = nil) {
+    public var taskTitle: String
+    
+    public init(taskTitle: String) {
         self.taskTitle = taskTitle
-        self.taskEstimatedDuration = taskEstimatedDuration
     }
 }
--- LiveActivities/FocusLiveActivity.swift
+++ LiveActivities/FocusLiveActivity.swift
@@ -2,32 +2,7 @@ import ActivityKit
 import WidgetKit
 import SwiftUI
 
-public struct FocusAttributes: ActivityAttributes {
-    public struct ContentState: Codable, Hashable {
-        public var sessionState: SessionState
-        public var elapsedSeconds: Int
-        public var totalEstimatedSeconds: Int?
-        
-        public init(sessionState: SessionState, elapsedSeconds: Int, totalEstimatedSeconds: Int? = nil) {
-            self.sessionState = sessionState
-            self.elapsedSeconds = elapsedSeconds
-            self.totalEstimatedSeconds = totalEstimatedSeconds
-        }
-    }
-    
-    public enum SessionState: String, Codable, Hashable {
-        case active = "Active"
-        case paused = "Paused"
-        case approachingTarget = "Approaching Target"
-        case completed = "Completed"
-    }
-    
-    public var taskTitle: String
-    
-    public init(taskTitle: String) {
-        self.taskTitle = taskTitle
-    }
-}
+
 
 public struct FocusLiveActivity: Widget {
     public init() {}
